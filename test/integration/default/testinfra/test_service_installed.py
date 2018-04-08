3376027d5b2282dc3797c6e5d592feb14b3cf4cc
J. Eduardo <j.eduardo@gmail.com>
2018-04-08 20:26:58 +0200

    Fixed python test methods to be prefixed with _

--- test/integration/default/testinfra/test_service_installed.py
+++ test/integration/default/testinfra/test_service_installed.py
@@ -1,19 +1,20 @@
 # Basic tests for the docker service
+import pytest
 
 
-def service_is_installed(host):
+def test_service_is_installed(host):
     docker = host.package("docker-ce")
     assert docker.is_installed
     assert docker.version.startswith("18.03")
 
 
-def service_is_running(host):
+def test_service_is_running(host):
     docker = host.service("docker")
     assert docker.is_running
     assert docker.is_enabled
 
 
-def systemd_service_is_changed(host):
+def test_systemd_service_is_changed(host):
     systemd = host.file("/lib/systemd/system/docker.service")
     assert "$DOCKER_OPTS" in systemd.content
     assert "EnvironmentFile" in systemd.content
